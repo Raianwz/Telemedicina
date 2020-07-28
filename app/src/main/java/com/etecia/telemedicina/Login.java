1cf2cef7a36852c27b9eff4ee3048052555033a2
raianwz <raian03@live.com>
2020-07-28 12:48:30 -0300

    Ligação entre as Telas Login e Menu (MainActivity)

--- app/src/main/java/com/etecia/telemedicina/Login.java
+++ app/src/main/java/com/etecia/telemedicina/Login.java
@@ -2,13 +2,47 @@ package com.etecia.telemedicina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Login extends AppCompatActivity {
+    ImageView RecebeImgLogin;
+    TextInputEditText txtEmail;
+    TextInputEditText txtSenha;
+
+    String LoginEstagiario = "estagiario@etecia.com.br";
+    String SenhaEstagiario = "estagiario";
+
+    String LoginPsiquiatra = "psiquiatra@etecia.com.br";
+    String SenhaPsiquiatra = "psiquiatra";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        RecebeImgLogin = findViewById(R.id.imgPaciente);
+
+        txtEmail = findViewById(R.id.txtEmail);
+        txtSenha = findViewById(R.id.txtSenha);
+
+    }
+
+    public void Entrar(View view) {
+        if (LoginEstagiario.equals(txtEmail.getText().toString()) && SenhaEstagiario.equals(txtSenha.getText().toString())) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.putExtra("loginE", 0);
+            startActivity(intent);
+        }
+
+        if (LoginPsiquiatra.equals(txtEmail.getText().toString()) && SenhaPsiquiatra.equals(txtSenha.getText().toString())) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.putExtra("loginP", 0);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/etecia/telemedicina/MainActivity.java
+++ app/src/main/java/com/etecia/telemedicina/MainActivity.java
@@ -2,12 +2,80 @@ package com.etecia.telemedicina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
+//Menu de Informações
 public class MainActivity extends AppCompatActivity {
+    //Estagiario
+    String avalicao = "Avaliação";
+    String psiquiatras = "Psiquiatras";
+    //Psiquiatra
+    String pacientes = "Pacientes";
+    String metodos = "Métodos Avaliativos";
+    MaterialToolbar navInfo;
+
+    GridView MenuInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Grid View Menu informações
+        MenuInfo = findViewById(R.id.MenuInfo);
+        AdapatorMenu adapatorMenu = new AdapatorMenu();
+        MenuInfo.setAdapter(adapatorMenu);
+
+        //Barra de informações
+        navInfo = findViewById(R.id.navInfo);
+
+        //navInfo.setLogo(R.drawable.ic_hospital);
+
+        //Intent que receberá os dados da ourta activity
+        Intent intent = getIntent();
+
+        String loginE = intent.getStringExtra("loginE");
+        String loginP = intent.getStringExtra("loginP");
+
     }
+    public class AdapatorMenu extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 1;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            Button item1;
+            Button item2;
+
+            View view1 = getLayoutInflater().inflate(R.layout.modelo_info, null);
+
+            item1 = view1.findViewById(R.id.btnAvaliacao);
+            item2 = view1.findViewById(R.id.btnPsiPac);
+            item1.setText(avalicao);
+            item2.setText(psiquiatras);
+
+            return view1;
+        }
+    }
+    //Fim do Menu
 }
